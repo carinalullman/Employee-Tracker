a4bcfc371e2565388aa1b75a4ea2b8456f6f3080
Carina Ullman <carinalullman@gmail.com>
2019-12-08 12:30:58 -0700

    Some changes but port is not working, need to repair

--- employee_db.sql
+++ employee_db.sql
@@ -3,8 +3,8 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 CREATE TABLE department (
-id INTEGER NOT NULL AUTO_INCREMENT,
-name VARCHAR(30) NULL,
+id INTEGER AUTO_INCREMENT NOT NULL ,
+dept_name VARCHAR(30) NULL,
 PRIMARY KEY (id)
 );
 
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var inquirer = require("inquirer");
-var consoleTable = require("console.table");
+//var consoleTable = require("console.table");
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
@@ -7,7 +7,7 @@ var connection = mysql.createConnection({
   port: 3030,
   user: "root",
   password: "root",
-  database: "employeeDB"
+  database: "employee_db"
 });
 
 connection.connect(function (err) {
@@ -94,7 +94,7 @@ function departmentView() {
       message: "View all employees by department",
     })
     .then(function (answer) {
-      var query = "SELECT name FROM department WHERE ?";
+      var query = "SELECT dept_name FROM department WHERE ?";
       connection.query(query, function (err, res) {
         for (var i = 0; i < res.length; i++) {
           console.log(res[i].department);
@@ -196,153 +196,153 @@ function employeeManager() {
       });
     });
   }
-//all this code below needs to be fixed to reflect a connection end
-  function exit() {
-    inquirer
-      .prompt([
-        {
-          name: "start",
-          type: "input",
-          message: "Enter starting position: ",
-          validate: function (value) {
-            if (isNaN(value) === false) {
-              return true;
-            }
-            return false;
-          }
-        },
-        {
-          name: "end",
-          type: "input",
-          message: "Enter ending position: ",
-          validate: function (value) {
-            if (isNaN(value) === false) {
-              return true;
-            }
-            return false;
-          }
-        }
-      ])
-      .then(function (answer) {
-        var query = "SELECT position,song,artist,year FROM top5000 WHERE position BETWEEN ? AND ?";
-        connection.query(query, [answer.start, answer.end], function (err, res) {
-          for (var i = 0; i < res.length; i++) {
-            console.log(
-              "Position: " +
-              res[i].position +
-              " || Song: " +
-              res[i].song +
-              " || Artist: " +
-              res[i].artist +
-              " || Year: " +
-              res[i].year
-            );
-          }
-          runSearch();
-        });
-      });
-  }
+// //all this code below needs to be fixed to reflect a connection end
+//   function exit() {
+//     inquirer
+//       .prompt([
+//         {
+//           name: "start",
+//           type: "input",
+//           message: "Enter starting position: ",
+//           validate: function (value) {
+//             if (isNaN(value) === false) {
+//               return true;
+//             }
+//             return false;
+//           }
+//         },
+//         {
+//           name: "end",
+//           type: "input",
+//           message: "Enter ending position: ",
+//           validate: function (value) {
+//             if (isNaN(value) === false) {
+//               return true;
+//             }
+//             return false;
+//           }
+//         }
+//       ])
+//       .then(function (answer) {
+//         var query = "SELECT position,song,artist,year FROM top5000 WHERE position BETWEEN ? AND ?";
+//         connection.query(query, [answer.start, answer.end], function (err, res) {
+//           for (var i = 0; i < res.length; i++) {
+//             console.log(
+//               "Position: " +
+//               res[i].position +
+//               " || Song: " +
+//               res[i].song +
+//               " || Artist: " +
+//               res[i].artist +
+//               " || Year: " +
+//               res[i].year
+//             );
+//           }
+//           runSearch();
+//         });
+//       });
+//   }
 
-  function songSearch() {
-    inquirer
-      .prompt({
-        name: "song",
-        type: "input",
-        message: "What song would you like to look for?"
-      })
-      .then(function (answer) {
-        console.log(answer.song);
-        connection.query("SELECT * FROM top5000 WHERE ?", { song: answer.song }, function (err, res) {
-          console.log(
-            "Position: " +
-            res[0].position +
-            " || Song: " +
-            res[0].song +
-            " || Artist: " +
-            res[0].artist +
-            " || Year: " +
-            res[0].year
-          );
-          runSearch();
-        });
-      });
-  }
+//   function songSearch() {
+//     inquirer
+//       .prompt({
+//         name: "song",
+//         type: "input",
+//         message: "What song would you like to look for?"
+//       })
+//       .then(function (answer) {
+//         console.log(answer.song);
+//         connection.query("SELECT * FROM top5000 WHERE ?", { song: answer.song }, function (err, res) {
+//           console.log(
+//             "Position: " +
+//             res[0].position +
+//             " || Song: " +
+//             res[0].song +
+//             " || Artist: " +
+//             res[0].artist +
+//             " || Year: " +
+//             res[0].year
+//           );
+//           runSearch();
+//         });
+//       });
+//   }
 
-  function songAndAlbumSearch() {
-    inquirer
-      .prompt({
-        name: "artist",
-        type: "input",
-        message: "What artist would you like to search for?"
-      })
-      .then(function (answer) {
-        var query = "SELECT top_albums.year, top_albums.album, top_albums.position, top5000.song, top5000.artist ";
-        query += "FROM top_albums INNER JOIN top5000 ON (top_albums.artist = top5000.artist AND top_albums.year ";
-        query += "= top5000.year) WHERE (top_albums.artist = ? AND top5000.artist = ?) ORDER BY top_albums.year, top_albums.position";
+//   function songAndAlbumSearch() {
+//     inquirer
+//       .prompt({
+//         name: "artist",
+//         type: "input",
+//         message: "What artist would you like to search for?"
+//       })
+//       .then(function (answer) {
+//         var query = "SELECT top_albums.year, top_albums.album, top_albums.position, top5000.song, top5000.artist ";
+//         query += "FROM top_albums INNER JOIN top5000 ON (top_albums.artist = top5000.artist AND top_albums.year ";
+//         query += "= top5000.year) WHERE (top_albums.artist = ? AND top5000.artist = ?) ORDER BY top_albums.year, top_albums.position";
 
-        connection.query(query, [answer.artist, answer.artist], function (err, res) {
-          console.log(res.length + " matches found!");
-          for (var i = 0; i < res.length; i++) {
-            console.log(
-              i + 1 + ".) " +
-              "Year: " +
-              res[i].year +
-              " Album Position: " +
-              res[i].position +
-              " || Artist: " +
-              res[i].artist +
-              " || Song: " +
-              res[i].song +
-              " || Album: " +
-              res[i].album
-            );
-          }
+//         connection.query(query, [answer.artist, answer.artist], function (err, res) {
+//           console.log(res.length + " matches found!");
+//           for (var i = 0; i < res.length; i++) {
+//             console.log(
+//               i + 1 + ".) " +
+//               "Year: " +
+//               res[i].year +
+//               " Album Position: " +
+//               res[i].position +
+//               " || Artist: " +
+//               res[i].artist +
+//               " || Song: " +
+//               res[i].song +
+//               " || Album: " +
+//               res[i].album
+//             );
+//           }
 
-          runSearch();
-        });
-      });
-  }
-  ////////////////////////////////////////////////////////
-  function promptUser() {
-    return inquirer.prompt(
-      {
-        type: "list",
-        name: "options",
-        message: "What would you like to do?",
-        choices: ["View all employees", "View all employees by department", "View all employees by manager", "Add Employee", "Remove Employee", "Update Employee Role", "Update Employee Manager"]
-        //need to add conditionals to whatever choice is selected and add addtional questions
-        //What is the employee's first name?
-        //What is the employee's last name?
-        //Which employee do you want to remove? - needs to bring up list of employees
-        //What role would you like to update it to?
-        //Who is the employee's manager?
-        //.then(answers => {
-        //  console.log("answer:", answers.choices);
-        // })
+//           runSearch();
+//         });
+//       });
+//   }
+//   ////////////////////////////////////////////////////////
+//   function promptUser() {
+//     return inquirer.prompt(
+//       {
+//         type: "list",
+//         name: "options",
+//         message: "What would you like to do?",
+//         choices: ["View all employees", "View all employees by department", "View all employees by manager", "Add Employee", "Remove Employee", "Update Employee Role", "Update Employee Manager"]
+//         //need to add conditionals to whatever choice is selected and add addtional questions
+//         //What is the employee's first name?
+//         //What is the employee's last name?
+//         //Which employee do you want to remove? - needs to bring up list of employees
+//         //What role would you like to update it to?
+//         //Who is the employee's manager?
+//         //.then(answers => {
+//         //  console.log("answer:", answers.choices);
+//         // })
 
-        // should wrap these "answers/choices" in an object?
+//         // should wrap these "answers/choices" in an object?
 
-        //  var department = { 
-        //     id 
-        //     dept_name 
-        //}
-        //  var role = {
-        //     id 
-        //     title 
-        //     salary 
-        //     department_id 
-        //}
-        // var employee = {
-        //     id
-        //     first_name 
-        //     last_name 
-        //     role_id 
-        //     manager_id
-        // }
-
-
+//         //  var department = { 
+//         //     id 
+//         //     dept_name 
+//         //}
+//         //  var role = {
+//         //     id 
+//         //     title 
+//         //     salary 
+//         //     department_id 
+//         //}
+//         // var employee = {
+//         //     id
+//         //     first_name 
+//         //     last_name 
+//         //     role_id 
+//         //     manager_id
+//         // }
 
 
 
-      });
-  }
+
+
+//       });
+//   }
